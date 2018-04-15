814b6dbb0f63085be5a2628f36fae550797a4d1b
Smekac <stojkic95@hotmail.com>
2018-04-15 12:54:02 +0200

    Dodat trenutno ulogovani korisnik ...

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,21 @@
 import {Component} from '@angular/core';
+import {KorisnikService} from '../korisnik.service';
+import {Korisnik} from '../models/korisnik';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent{
 
   // currentJustify = 'start';
+  private korisnik: Korisnik;
+
+
+  constructor( private korisnikService: KorisnikService) {
+    this.korisnik = JSON.parse(localStorage.getItem('ulogovaniKorisnik'));
+
+  }
+
 }
