b3ecea64c5a9fcc498b9d6a02a0cbbe72a0ab493
Smekac <stojkic95@hotmail.com>
2018-04-16 21:39:59 +0200

    doraditi getBids

--- src/app/fan-zone/my-ads/my-ads.component.ts
+++ src/app/fan-zone/my-ads/my-ads.component.ts
@@ -1,5 +1,5 @@
 
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {PropService} from '../prop.service';
 import {Router} from '@angular/router';
 import {Bid, UsedProp} from '../../models/prop';
@@ -14,13 +14,24 @@ export class MyAdsComponent implements OnInit {
 
   usedProps: UsedProp[];
   korisnik: Korisnik;
+  // bids: Bid[];       // Kad imamo <app-bid-list [bids]="bids"></app-bid-list>
+  usedPropId: number;
+  biddingFinished: boolean;
+  creatorUsedProp: boolean;
+
+  @Input() bids: Bid[];
+ // usedProp: UsedProp;
+
+  idKoriscenogRekvizita: number;
 
   constructor(private propService: PropService, private router: Router) {
     this.korisnik =  JSON.parse(localStorage.getItem('ulogovaniKorisnik'));
-
+   // this.usedProps =new UsedProp();
   }
 
   ngOnInit() {
+    this.creatorUsedProp = this.propService.creatorUsedProp;
+    this.biddingFinished = this.propService.biddingFinished;
     this.getMyAds();
   }
 
@@ -31,11 +42,27 @@ export class MyAdsComponent implements OnInit {
       });
   }
 
+
+  acceptBid(bidId: number) {
+        console.log("____________" + this.idKoriscenogRekvizita)
+    this.propService.acceptBid(this.idKoriscenogRekvizita, bidId)
+      .subscribe(resp => {
+        if (resp.status === 204) {
+          alert('Uspesno ste izabrali pobednika licitacije');
+          // this.router.navigate(['fanpage/my-ads']);
+          window.location.reload();
+        } else {
+          alert('Greska!');
+        }
+      });
+  }
+
   deleteUsedProp(id: number) {
     this.propService.deleteUsedProp(id)
       .subscribe(resp => {
         if (resp.status === 204) {
           alert('Uspesno obrisan oglas i sve licitacije');
+          window.location.reload();
         } else {
           alert('Nije uspelo brisanje oglasa, doslo je do greske.');
         }
@@ -43,6 +70,7 @@ export class MyAdsComponent implements OnInit {
   }
 
   getBids(usedPropId: number, usedPropBid: number) {
+    this.idKoriscenogRekvizita = usedPropId;
     if (usedPropBid) {
       this.propService.biddingFinished = true;
     } else {
@@ -50,15 +78,19 @@ export class MyAdsComponent implements OnInit {
     }
     this.propService.getBids(usedPropId).subscribe(
       (data: Bid[]) => {
+        this.bids = data;
+      },
+      (data: Bid[]) => {
         if (data.length === 0) {
           alert('Za odabrani oglas nema ni jedne ponude');
         } else {
           this.propService.bids = data;
           this.propService.creatorUsedProp = true;
-          this.router.navigate(['/fanpage/used-prop/' + usedPropId + '/bids']);
+        //  this.router.navigate(['/fanpage/used-prop/' + usedPropId + '/bids']);
         }
       },
-      err => console.error(err)
+
+     // err => console.error(err)
     );
   }
 
